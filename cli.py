6d8242acd26291965351ec00532168580ab433cc
Sabutobi <peter.lozhkin@gmail.com>
2019-01-06 12:34:15 +0200

    cleanup commented

--- cli.py
+++ cli.py
@@ -1,10 +1,5 @@
 import greet
 import cProfile
-# import ptvsd
-# port = 3000
-# ptvsd.enable_attach(address=('localhost', port))
-# ptvsd.wait_for_attach()
-
 
 def main():
     greet.greet("dkjsjaldnjls")
